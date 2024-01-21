612373d157522922dfe1252d30291cdd0835741d
Justin Joonsoo Lee <joonsmoons@gmail.com>
2024-01-21 17:20:49 +0900

    Add transform pipeline (DAG) and split tables

--- app/etl/assets/sql/transform/staging_flights.sql
+++ app/etl/assets/sql/transform/staging_flights.sql
@@ -1,23 +1,22 @@
-with newflight_price as (
-	SELECT DATE_PART('year', DATE("departureDate")) as departureYear
+
+SELECT DATE_PART('year', DATE("departureDate")) as departureYear
 	, DATE_PART('month', DATE("departureDate")) as departureMonth
 	, DATE_PART('day', DATE("departureDate")) as departureDay
-	, EXTRACT(isodow from DATE("departureDate")) as depatureDayOfWeek
+	, CASE EXTRACT(isodow from DATE("departureDate"))
+        WHEN 1 THEN 'Monday'
+        WHEN 2 THEN 'Tuesday'
+        WHEN 3 THEN 'Wednesday'
+        WHEN 4 THEN 'Thursday'
+        WHEN 5 THEN 'Friday'
+        WHEN 6 THEN 'Saturday'
+        WHEN 7 THEN 'Sunday'
+    	END as departureDayOfWeek
 	-- 1 is Monday, 7 is Sunday 
 	, CAST("cheapestPrice" AS NUMERIC) as price
-	, CAST(duration AS NUMERIC)
-	, *
-	FROM public.flight_price
-						 )
-						 
--- Table 1						 
-select count(*)as numberofflight, round(avg(price),1) as averageprice, depatureDayOfWeek, origin, destination
-	from newflight_price
-	group by depatureDayOfWeek, origin, destination
-	order by destination
-	
--- Table 2
-select count(*)as numberofflight, round(avg(price),1) as averageprice, departureMonth, origin, destination
-	from newflight_price
-	group by departureMonth, origin, destination
-	order by destination
+	, CAST(duration AS NUMERIC) as duration
+	, "viewedAt"
+	, origin
+	, destination
+	, "departureDate"
+	, "returnDate"
+FROM flight_price
---
+++ app/etl/pipelines/transform.py
@@ -0,0 +1,93 @@
+from etl.connectors.postgresql import PostgreSqlClient
+from jinja2 import Environment, FileSystemLoader
+from etl.assets.transform import SqlTransform, transform
+from dotenv import load_dotenv
+import os
+import yaml
+from pathlib import Path
+from etl.assets.pipeline_logging import PipelineLogging
+from graphlib import TopologicalSorter
+
+
+def run_pipeline(pipeline_config: dict):
+    load_dotenv()
+
+    DB_USERNAME = os.environ.get("DB_USERNAME")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    SERVER_NAME = os.environ.get("SERVER_NAME")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    PORT = os.environ.get("PORT")
+
+    postgresql_client = PostgreSqlClient(
+        server_name=SERVER_NAME,
+        database_name=DATABASE_NAME,
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        port=PORT,
+    )
+
+    # get config variables
+    yaml_file_path = __file__.replace(".py", ".yaml")
+    if Path(yaml_file_path).exists():
+        with open(yaml_file_path) as yaml_file:
+            pipeline_config = yaml.safe_load(yaml_file)
+            PIPELINE_NAME = pipeline_config.get("name")
+            logger = PipelineLogging(
+                pipeline_name=PIPELINE_NAME, postgresql_client=postgresql_client
+            )
+    else:
+        raise Exception(
+            f"Missing {yaml_file_path} file! Please create the yaml file with at least a `name` key for the pipeline name."
+        )
+
+    try:
+        transform_template_environment = Environment(
+            loader=FileSystemLoader("etl/assets/sql/transform")
+        )
+
+        # create nodes
+        staging_flights = SqlTransform(
+            table_name="staging_flights",
+            postgresql_client=postgresql_client,
+            environment=transform_template_environment,
+        )
+        serving_avg_price_dow = SqlTransform(
+            table_name="serving_avg_price_dow",
+            postgresql_client=postgresql_client,
+            environment=transform_template_environment,
+        )
+        serving_avg_price_month = SqlTransform(
+            table_name="serving_avg_price_month",
+            postgresql_client=postgresql_client,
+            environment=transform_template_environment,
+        )
+
+        # create DAG
+        dag = TopologicalSorter()
+        dag.add(staging_flights)
+        dag.add(serving_avg_price_dow, staging_flights)
+        dag.add(serving_avg_price_month, staging_flights)
+
+        logger.log_message(
+            print,
+            message="Transform data from all APIs",
+            process="[Transform] Transform data",
+            output="START",
+        )
+
+        # run transform
+        transform(dag=dag)
+
+        logger.log_message(
+            print,
+            message="Transform data from all APIs",
+            process="[Transform] Transform data",
+            output="SUCCESS",
+        )
+    except BaseException as e:
+        logger.log_message(
+            print,
+            message=f"Transform pipeline run failed. See detailed logs: {e}",
+            process="[Transform] Transform Pipeline Failed",
+            output="FAIL",
+        )
