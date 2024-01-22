223c5fbd7a86923d2aed729ec9def365313877eb
zazwaz12 <timothyszandi@gmail.com>
2024-01-22 20:29:55 +0800

    The unit testing for the flight_api connector

--- app/etl/connectors/test_postgresql.py
+++ app/etl/connectors/test_postgresql.py
@@ -1,19 +1,19 @@
 import unittest
 import sqlalchemy
-from postgresql import PostgreSqlClient  # Make sure to replace 'your_module' with the actual module where get_engine is defined
+from etl.connectors.postgresql import PostgreSqlClient  # Make sure to replace 'your_module' with the actual module where get_engine is defined
 
 class PostgreSql(unittest.TestCase):
         def test_get_engine(self):
-            # Create an instance of PostgreSqlClient with the necessary parameters
+            # Creates an instance of PostgreSqlClient with the necessary parameters
             postgres_client = PostgreSqlClient(
                 server_name='flights-db-1.clys4ao8guak.ca-central-1.rds.amazonaws.com',
                 database_name="flights",
                 username="postgres",
                 password="flights010",
-                port=5432  # Update with your actual port
+                port=5432
             )
 
-            # Call the get_engine method and check the type
+            # Calls the get_engine method and check the type
             engine = postgres_client.get_engine()
             self.assertIs(type(engine.connect()), sqlalchemy.engine.Connection)
         
